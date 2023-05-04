e09e23dff62121e1e15fc62a40060a63c2b2c5ed
Jiří Michna <jirikmichna@seznam.cz>
2023-05-04 15:44:42 +0200

    huh

---
+++ pisemka.py
@@ -0,0 +1,22 @@
+#1
+for i in range(1, 11):
+    print(i * 3)
+
+#2
+A = 69
+B = 29
+
+if A > B:
+    print("A je větší než B.")
+elif A < B:
+    print("A je menší než B.")
+elif A == B:
+    print("A i B si jsou rovné.")
+
+#3
+cislo = int(input("Zadejte číslo: "))
+for i in range(0, 21):
+    print(i * cislo)
+
+#4
+cisla = [8, 176, 29, 43, 848, 64, 152, 17, 205, 56]
